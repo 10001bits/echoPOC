6f54454cac56ee711ed72d694c8087adf8ce09a5
arc1999 <ayushrajchauhan99@gmail.com>
2020-09-17 00:51:38 +0530

    crud operations

---
+++ types/User.go
@@ -0,0 +1,11 @@
+package types
+import (
+	"github.com/Kamva/mgm"
+)
+
+type User struct {
+	mgm.DefaultModel `bson:",inline"`
+	Name      string `json:"name" bson:"name"`
+	Email	string	`json:"email" bson:"email"`
+	Age 	int 	`json:"age" bson:"age"`
+}
